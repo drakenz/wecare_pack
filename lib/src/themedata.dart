4aed42d8b382d09540edbb6e2f3cd29c40e5fe6c
drakenz <drakenz2010@gmail.com>
2022-07-13 22:24:49 +0200

    More Work For Background

--- lib/src/themedata.dart
+++ lib/src/themedata.dart
@@ -2,18 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 //Gradient Background
-Widget backgroundColors(BuildContext ctx, Widget child) {
-  return Container(
-    width: MediaQuery.of(ctx).size.width,
-    height: MediaQuery.of(ctx).size.height,
-    decoration: const BoxDecoration(
-        gradient: LinearGradient(
-      colors: [Color(0xff00ffcf), Color(0xff8250FA)],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-    )),
-    child: child,
-  );
+
+class Background extends StatefulWidget {
+  final BuildContext ctx;
+  final Widget child;
+  const Background({Key? key, required this.ctx, required this.child})
+      : super(key: key);
+
+  @override
+  State<Background> createState() => _BackgroundState();
+}
+
+class _BackgroundState extends State<Background> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(widget.ctx).size.width,
+      height: MediaQuery.of(widget.ctx).size.height,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Color(0xff00ffcf), Color(0xff8250FA)],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      )),
+      child: widget.child,
+    );
+  }
 }
 //Card Component Shown on the start screen on both versions
 
