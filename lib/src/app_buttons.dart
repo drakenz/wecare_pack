619394af991f926504e9cce0ba4cb9b4e15fe865
drakenz <drakenz2010@gmail.com>
2022-07-13 23:09:28 +0200

    Added Gradient Button

--- lib/src/app_buttons.dart
+++ lib/src/app_buttons.dart
@@ -30,3 +30,39 @@ class RoundedButton extends StatelessWidget {
     );
   }
 }
+
+class GradientRoundedButton extends StatelessWidget {
+  final buttonText;
+  final buttonRadius;
+  final void Function()? onPressed;
+  final List<Color> ColorList;
+  const GradientRoundedButton(
+      {Key? key,
+      this.buttonText,
+      this.buttonRadius,
+      this.onPressed,
+      required this.ColorList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(buttonRadius),
+            gradient: LinearGradient(
+                colors: ColorList,
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight)),
+        child: ElevatedButton(
+            onPressed: onPressed,
+            child: Text(
+              buttonText,
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(buttonRadius))),
+              backgroundColor: MaterialStateProperty.all(Colors.transparent),
+            )));
+  }
+}
