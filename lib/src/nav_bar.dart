6683b4de479d39934ac31a7b7daa5786970d5ce0
drakenz <drakenz2010@gmail.com>
2022-07-14 06:27:43 +0200

    Work on nav_bar

--- lib/src/nav_bar.dart
+++ lib/src/nav_bar.dart
@@ -38,7 +38,8 @@ class _NavBarState extends State<NavBar> with SingleTickerProviderStateMixin {
                 scale: Tween(begin: 0.75, end: 2.0).animate(CurvedAnimation(
                     parent: _controller, curve: Curves.bounceIn)),
                 child: Container(
-                  decoration: const BoxDecoration(shape: BoxShape.circle),
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.black),
                   child: const Icon(Icons.person),
                 ),
               )),
